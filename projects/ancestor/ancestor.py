d48ee2181ffd5f24bb533faa6bbed05b5ada6b19
AwaMelvine <melvineawa@gmail.com>
2019-11-05 16:29:44 +0100

    Start solving ancestor exercise

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,39 @@
+from graph import Graph
+from util import Stack
+
+def createGraph(ancestors):
+    pass
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    gg = Graph()
+    for person in ancestors:
+        gg.add_vertex(person[1])
+        gg.add_vertex(person[0])
+
+    for person in ancestors:
+        gg.add_edge(person[1], person[0])
+    print(gg.vertices)
+
+    stack, path = Stack(), []
+    stack.push(starting_node)
+
+    while stack:
+        vertex = stack.pop()
+        print(f"{vertex}-")
+        if vertex in path:
+            continue
+        path.append(vertex)
+
+        if vertex is None:
+            return path[len(path) - 2]
+
+        for adj_vertex in gg.vertices[vertex]:
+            stack.push(adj_vertex)
+
+    return -1
+
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(f"{earliest_ancestor(test_ancestors, 2)} ===")
